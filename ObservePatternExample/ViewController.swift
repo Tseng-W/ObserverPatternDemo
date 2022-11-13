ea06ff66a2652454caaae66839cade36bbb8162c
Wen <twayne0618@gmail.com>
2022-11-13 20:10:10 +0800

    Initial Commit

---
+++ ObservePatternExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObservePatternExample
+//
+//  Created by 曾問 on 2022/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
